1d47c58fa2d111d9376587ca2b3fd6fc2deaae1e
Wichai Bubpamataning <wichai.b@lexnetix.co.th>
2022-09-15 23:05:42 +0700

    modal input name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { JitsiMeeting } from "@jitsi/react-sdk";
 import {
   Button,
-  Stack,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
   Grid,
   Container,
   TextField,
@@ -13,7 +16,7 @@ import {
 } from "@mui/material";
 
 const App = () => {
-  const displayName = "นายวิชัย บุบผามะตะนัง";
+  const [displayName, setDisplayName] = React.useState("วิชัย บุบผามะตะนัง");
   const [muted, setMuted] = React.useState(false);
   const [api, setApi] = React.useState(null);
   const [participantsInfo, setParticipantsInfo] = React.useState(null);
@@ -22,124 +25,179 @@ const App = () => {
     api.executeCommand(command);
   };
 
+  // open dialog
+  const [open, setOpen] = React.useState(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <Container maxWidth="xl">
-        <Grid container spacing={2}>
-          <Grid item container spacing={2} xs={10}>
-            <Grid item xs={12}>
-              <Typography variant="body1">
-                สถานะไมค์ : {muted ? "เปิดไมค์" : "ปิดไมค์"}
-              </Typography>
+      {!open && (
+        <Container maxWidth="xl">
+          <Grid container spacing={2}>
+            <Grid item container spacing={2} xs={10}>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  สถานะไมค์ : {muted ? "เปิดไมค์" : "ปิดไมค์"}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <JitsiMeeting
+                  domain={`testjitsi.ddns.net`}
+                  roomName="react_demo"
+                  configOverwrite={{
+                    startWithAudioMuted: true,
+                    disableModeratorIndicator: false,
+                    startScreenSharing: true,
+                    enableEmailInStats: false,
+                    disableSelfView: false,
+                    apiLogLevels: ["warn"],
+                    filmstrip: {
+                      disableResizable: true,
+                      disableStageFilmstrip: true,
+                      disableFilmstripAutohiding: false,
+                      doNotFlipLocalVideo: true,
+                    },
+                  }}
+                  spinner={() => <h1>รอสักครู่....</h1>}
+                  interfaceConfigOverwrite={{
+                    VIDEO_LAYOUT_FIT: "nocrop",
+                    DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
+                    // TILE_VIEW_MAX_COLUMNS: 1,
+                    CLOSE_PAGE_GUEST_HINT: true,
+                    HIDE_INVITE_MORE_HEADER: true,
+                    // DEFAULT_BACKGROUND: "#BDF0FF",
+                    // VERTICAL_FILMSTRIP: false,
+                  }}
+                  userInfo={{
+                    email: "wichai.b@lexnetix.co.th",
+                    displayName,
+                  }}
+                  onApiReady={async (externalApi) => {
+                    externalApi.on("audioMuteStatusChanged", (state) => {
+                      setMuted(!state.muted);
+                    });
+
+                    externalApi.on("raiseHandUpdated", (state) => {
+                      alert("มีผู้ประท้วง");
+                    });
+
+                    // const displayName = externalApi.getDisplayName(participantId);
+                    // console.log(displayName);
+
+                    externalApi.executeCommand(
+                      "avatarUrl",
+                      "https://i.pinimg.com/236x/cf/74/1a/cf741a56f200041630642387a61ac31c.jpg"
+                    );
+                    externalApi.executeCommand("overwriteConfig", {
+                      toolbarButtons: [],
+                    });
+
+                    // here you can attach custom event listeners to the Jitsi Meet External API
+                    // you can also store it locally to execute commands
+                    setApi(externalApi);
+
+                    const api = await externalApi.getParticipantsInfo();
+                    console.warn(api);
+                  }}
+                  getIFrameRef={(iframeRef) => {
+                    iframeRef.style.height = "80vh";
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} container columnGap={2}>
+                <Button
+                  variant="contained"
+                  onClick={() => handleExcuteCommand("toggleVideo")}
+                >
+                  ปิด/เปิด กล้อง
+                </Button>
+
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    handleExcuteCommand("toggleAudio");
+                    // handleExcuteCommand("toggleSubtitles");
+                  }}
+                >
+                  ปิด/เปิด ไมค์
+                </Button>
+
+                <Button
+                  variant="contained"
+                  onClick={() => handleExcuteCommand("toggleFilmStrip")}
+                >
+                  ปิด/เปิด ผู้ประชุม
+                </Button>
+
+                <Button
+                  variant="contained"
+                  onClick={() => handleExcuteCommand("toggleRaiseHand")}
+                >
+                  ยกมือ
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <JitsiMeeting
-                domain={`testjitsi.ddns.net`}
-                roomName="test"
-                configOverwrite={{
-                  startWithAudioMuted: true,
-                  disableModeratorIndicator: false,
-                  startScreenSharing: true,
-                  enableEmailInStats: false,
-                  disableSelfView: false,
-                  // disableFilmstripAutohiding: true,
-                  filmstrip: {
-                    disableResizable: true,
-                    minParticipantCountForTopPanel: 50,
-                  },
-                }}
-                spinner={() => <h1>รอสักครู่....</h1>}
-                interfaceConfigOverwrite={{
-                  VIDEO_LAYOUT_FIT: "nocrop",
-                  DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
-                  // DEFAULT_BACKGROUND: "#BDF0FF",
-                  // VERTICAL_FILMSTRIP: false
-                }}
-                userInfo={{
-                  email: "wichai.b@lexnetix.co.th",
-                  displayName,
-                }}
-                onApiReady={(externalApi) => {
-                  externalApi.on("audioMuteStatusChanged", (state) => {
-                    setMuted(!state.muted);
-                  });
+            <Grid item xs={2}>
+              <List>
+                {(participantsInfo ? participantsInfo.participants : []).map(
+                  (data, index) => {
+                    return (
+                      <React.Fragment key={index}>
+                        <ListItem>
+                          <ListItemText
+                            primary={data.displayName}
+                            secondary={data.id}
+                          />
+                        </ListItem>
+                      </React.Fragment>
+                    );
+                  }
+                )}
+              </List>
+            </Grid>
+          </Grid>
+        </Container>
+      )}
 
-                  externalApi.on("raiseHandUpdated", (state) => {
-                    alert("มีผู้ประท้วง");
-                  });
-
-                  externalApi.executeCommand(
-                    "avatarUrl",
-                    "https://i.pinimg.com/236x/cf/74/1a/cf741a56f200041630642387a61ac31c.jpg"
-                  );
-                  externalApi.executeCommand("overwriteConfig", {
-                    toolbarButtons: [],
-                  });
-
-                  // externalApi.get
-
-                  // here you can attach custom event listeners to the Jitsi Meet External API
-                  // you can also store it locally to execute commands
-                  setApi(externalApi);
-                }}
-                getIFrameRef={(iframeRef) => {
-                  iframeRef.style.height = "80vh";
-                }}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+          <Typography variant="body1">ตั้งค่าใช้งานระบบ</Typography>
+        </DialogTitle>
+        <DialogContent sx={{ width: "550px" }}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item xs={9} md={10}>
+              <TextField
+                placeholder="ระบุชื่อ"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+                fullWidth
               />
             </Grid>
-            <Grid item xs={12} container columnGap={2}>
+            <Grid item xs={3} md={2}>
               <Button
                 variant="contained"
-                onClick={() => handleExcuteCommand("toggleVideo")}
-              >
-                ปิด/เปิด กล้อง
-              </Button>
-
-              <Button
-                variant="contained"
+                sx={{ height: "50px", width: "80px" }}
                 onClick={() => {
-                  handleExcuteCommand("toggleAudio");
-                  // handleExcuteCommand("toggleSubtitles");
+                  if (displayName == "") {
+                    alert("ระบุข้อมูลชื่อผู้ใช้งาน!");
+                  } else {
+                    setOpen(false);
+                  }
                 }}
               >
-                ปิด/เปิด ไมค์
-              </Button>
-
-              <Button
-                variant="contained"
-                onClick={() => handleExcuteCommand("toggleFilmStrip")}
-              >
-                ปิด/เปิด ผู้ประชุม
-              </Button>
-
-              <Button
-                variant="contained"
-                onClick={() => handleExcuteCommand("toggleRaiseHand")}
-              >
-                ยกมือ
+                <Typography variant="body1">ตกลง</Typography>
               </Button>
             </Grid>
           </Grid>
-          <Grid item xs={2}>
-            <List>
-              {(participantsInfo ? participantsInfo.participants : []).map(
-                (data, index) => {
-                  return (
-                    <React.Fragment key={index}>
-                      <ListItem>
-                        <ListItemText
-                          primary={data.displayName}
-                          secondary={data.id}
-                        />
-                      </ListItem>
-                    </React.Fragment>
-                  );
-                }
-              )}
-            </List>
-          </Grid>
-        </Grid>
-      </Container>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
